2efff0524f85c3bb509d1e58984fb69953bd57a0
gcsawnam <gcsawnam@gmail.com>
2022-01-25 09:34:11 +0545

    test

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# website
+# button-hover
---
+++ index.js
@@ -0,0 +1,14 @@
+function changeBackcolor(color) {
+    document.body.style.background = color;
+}
+
+function changeBackcolor1(color) {
+    document.body.style.background = color;
+}
+
+function changeBackcolor2(color) {
+    document.body.style.background = color;
+}
+function changebackgroundimage() {
+    // document.getElementById("btn").body.style.backgroundImage = "url(sanam.jpg.jpg)";
+}
