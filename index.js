3dfb77258c70d51919e0932b5db65e97b993d105
gcsawnam <gcsawnam@gmail.com>
2022-01-28 14:22:43 +0545

    test

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function fontcolor(x)
 {
 document.getElementById("demo").style.color = x;
 }
+
+
+ function moveright() {
+    const img = document.getElementById("car1");
+     img.style.left = `${img.offsetLeft + 10}px`;
+  }
+//   function moveleft() {
+//     const img = document.getElementById('car1');
+//     img.style.left = Number(img.style.left.slice(0, -2)) - 10 + 'px';
+//   }
